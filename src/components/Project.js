c8c406f003628da23d035bc1db36438b077abcfe
rendi12345678 <hardleberg@gmail.com>
2023-12-12 10:28:57 +0700

    Remove Margin Top in hero

--- src/components/Project.js
+++ src/components/Project.js
@@ -25,25 +25,25 @@ const Project = () => {
               Laudantium dolorem animi provident voluptatem temporibus.
             </figcaption>
             <div>
-              <button className="view-code-btn">Code</button>
+              <button className="view-code-btn">Github Code</button>
               <button className="demo-btn">Demo</button>
             </div>
           </div>
         </figure>
         <figure className="project">
           <img
-            src="/img/eletype.png"
+            src="/img/e1.png"
             alt="Project Image"
             title="Project Image"
           />
           <div>
-            <h3>Typing Test Game</h3>
+            <h3>Online Shop</h3>
             <figcaption>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Laudantium dolorem animi provident voluptatem temporibus.
             </figcaption>
             <div>
-              <button className="view-code-btn">Code</button>
+              <button className="view-code-btn">Github Code</button>
               <button className="demo-btn">Demo</button>
             </div>
           </div>
