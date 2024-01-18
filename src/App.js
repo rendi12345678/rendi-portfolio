878ef9d75a0de690e0f2fba89257a1a1c8e89261
rendi12345678 <hardleberg@gmail.com>
2024-01-18 23:06:35 +0800

    Merapikan code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import {Routes, Route} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import './styles/reset.css';
-import './styles/navbar.css';
-import './styles/body.css';
-import './styles/hero.css';
-import Home from './Pages/Home';
-import './App.css';
-import Footer from './components/Footer';
-import Projects from './Pages/Projects';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./styles/reset.css";
+import "./styles/navbar.css";
+import "./styles/body.css";
+import "./styles/hero.css";
+import Home from "./Pages/Home";
+import "./App.css";
+import Footer from "./components/Footer";
+import Projects from "./Pages/Projects";
 
 function App() {
   return (
     <div className="App">
-       <Navbar/>
-       <Routes>
-          <Route path='/' 
-                 exact 
-                 element={<Home/>}/>
-          <Route path='/projects' 
-                 exact 
-                 element={<Projects/>}/>
-       </Routes>
-       <Footer/>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/projects" exact element={<Projects />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
