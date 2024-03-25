c282d145ab75bf2beb28b1b40d88b54589f5e9bf
rendi12345678 <hardleberg@gmail.com>
2024-03-26 00:23:28 +0800

    Made it responsive

---
+++ src/components/ui/HamburgerMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HamburgerMenu() {
+  return (
+    <div className="hamburger-menu">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
+
+export default HamburgerMenu;
--- src/components/ui/NavList.js
+++ src/components/ui/NavList.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HamburgerMenu from "./HamburgerMenu";
 import List from "./List";
 
 function NavList({ children }) {
   return (
     <nav>
       <List>{children}</List>
+      <HamburgerMenu />
     </nav>
   );
 }
