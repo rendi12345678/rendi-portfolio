1d4136ba3e6e8f807ad05c8e9ac808562d815a68
rendi12345678 <hardleberg@gmail.com>
2024-04-04 23:42:54 +0800

    Improve UI

--- src/components/main/Portfolio.js
+++ src/components/main/Portfolio.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../assets/styles/portfolio.css";
 import Container from "../ui/Container";
 import Title from "../ui/Title";
 import ListPortfolio from "./../ui/ListPortfolio";
 
 function Portfolio() {
+  useEffect(() => {
+    document.querySelector(".swiper-pagination-bullet").classList.add("reveal");
+  }, []);
+
   return (
     <section className="portfolio reveal" id="portfolio">
       <Container>
--- src/components/ui/LinkItem.js
+++ src/components/ui/LinkItem.js
@@ -1,13 +1,15 @@
 import React, { useRef } from "react";
 import { HashLink } from "react-router-hash-link";
 import useDelay from "../../hooks/useDelay";
+import useNavbar from "../../hooks/useNavbar";
 
 function LinkItem({ to, text, delay }) {
+  const { toggleNav } = useNavbar();
   const liRef = useRef(null);
   useDelay({ delay, ref: liRef });
 
   return (
-    <li ref={liRef} className="reveal">
+    <li ref={liRef} className="reveal" onClick={toggleNav}>
       <HashLink to={to} smooth>
         {text}
       </HashLink>
