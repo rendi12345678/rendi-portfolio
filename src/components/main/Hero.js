1f5aa4779b5fe296c59d77cc35402e853346ca39
rendi12345678 <hardleberg@gmail.com>
2024-04-05 23:58:02 +0800

    Optimize images

--- src/components/main/Hero.js
+++ src/components/main/Hero.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../assets/styles/hero.css";
 import Container from "../ui/Container";
+import HeroImage from "../ui/HeroImage";
 import HeroInfo from "../ui/HeroInfo";
 
 function Hero() {
   return (
     <section id="hero" className="hero">
+      <HeroImage />
       <Container>
         <HeroInfo />
       </Container>
