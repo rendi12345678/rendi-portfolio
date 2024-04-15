5e35af2c5c7fc852cdcae9fe1706a4b3c96a6465
rendi12345678 <hardleberg@gmail.com>
2024-04-15 11:44:30 +0800

    Integrate css to sass

--- src/components/ui/HeroImage.js
+++ src/components/ui/HeroImage.js
@@ -5,7 +5,7 @@ function HeroImage() {
   return (
     <picture className="hero-image reveal">
       <ImageComponent
-        src="/images/background1-1300.webp"
+        src="/images/background1.webp"
         hash="LfEV?3%MD%of~q%MWARj?bxaofj]"
         alt="lake"
       />
